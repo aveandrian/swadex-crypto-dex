f3352434390a82108de96c45b922eb7a7099d07e
aveandrian <trancemilly7@gmail.com>
2024-02-20 14:03:08 +0100

    adds base_url variable

--- crypto-dex-fe/src/api.js
+++ crypto-dex-fe/src/api.js
@@ -1,13 +1,14 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
+const baseURL = import.meta.env.VITE_BASE_URL
 
 export async function getTokensList(){
-    let res = await fetch('http://localhost:3000/tokenList')
+    let res = await fetch(`${baseURL}/tokenList`)
     let data = await res.json()
     return data.tokens
 }
 
 export async function getRates(fromAddress, toAddress, weiAmount){
-    let res = await fetch(`http://localhost:3000/rates/${fromAddress}/${toAddress}/${weiAmount}`)
+    let res = await fetch(`${baseURL}/rates/${fromAddress}/${toAddress}/${weiAmount}`)
     let rates = await res.json()
     let toAmount = rates.toAmount / Math.pow(10, rates.toToken.decimals)
     return parseFloat(toAmount).toFixed(4)
@@ -15,20 +16,20 @@ export async function getRates(fromAddress, toAddress, weiAmount){
 
 export async function dexSwap(fromToken, fromAmount, toToken, wallet, slippage){
     try {
-        let checkAllowanceRes = await fetch(`http://localhost:3000/checkAllowance?tokenAddress=${fromToken.address}&wallet=${wallet}`)
+        let checkAllowanceRes = await fetch(`${baseURL}/checkAllowance?tokenAddress=${fromToken.address}&wallet=${wallet}`)
         let data = await checkAllowanceRes.json()
         if(parseFloat(data.allowance) / Math.pow(10, fromToken.decimals) < parseFloat(fromAmount)){
             console.log("Need more allowance")
             await delay(2000)
             let amountInWei = parseFloat(fromAmount) * Math.pow(10, fromToken.decimals)
-            let txForApprovalRes = await fetch(`http://localhost:3000/buildAllowanceTx?tokenAddress=${fromToken.address}&amount=${amountInWei}`)
+            let txForApprovalRes = await fetch(`${baseURL}/buildAllowanceTx?tokenAddress=${fromToken.address}&amount=${amountInWei}`)
             let txForApprovalData = await txForApprovalRes.json()
             return txForApprovalData
         }
         console.log("Swapping!")
         await delay(2000)
         let amountInWei = parseFloat(fromAmount) * Math.pow(10, fromToken.decimals)
-        let txSwapRes = await fetch(`http://localhost:3000/buildSwapTx?fromToken=${fromToken.address}&toToken=${toToken.address}&amount=${amountInWei}&from=${wallet}&slippage=${slippage}`)
+        let txSwapRes = await fetch(`${baseURL}/buildSwapTx?fromToken=${fromToken.address}&toToken=${toToken.address}&amount=${amountInWei}&from=${wallet}&slippage=${slippage}`)
         let txSwapData = await txSwapRes.json()
         return txSwapData
     }catch(e){
@@ -38,6 +39,6 @@ export async function dexSwap(fromToken, fromAmount, toToken, wallet, slippage){
 }
 
 export async function getTokenBalances(address){
-    let res = await fetch(`http://localhost:3000/balances/${address}`)
+    let res = await fetch(`${baseURL}/balances/${address}`)
     return await res.json()
 }
